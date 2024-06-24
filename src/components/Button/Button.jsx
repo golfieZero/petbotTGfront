be44cbe48476a783968a19a90caacffe915e944c
golfieZero <vladislav.morozov0@inbox.ru>
2024-06-24 17:40:31 +0300

    WIP

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Button = () => {
+    return (
+        <button {...props} className={'button' + props.className} />
+            
+    )
+}
+
+export default Button;
